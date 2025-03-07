5443a33f9bd91d0fd93a9f3d356f1a7fda6c2ae7
Tanyboy3333 <tanveer.24mas10044@vitbhopal.ac.in>
2025-03-07 14:33:50 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,27 +1,14 @@
 import unittest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
-
 from app import app
 
+class FlaskTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
 
-class FlaskTestCase(unittest.TestCase):
-    def test_home(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
+    def test_homepage(self):
+        response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_prediction(self):
-        tester = app.test_client(self)
-        response = tester.post('/predict', data=dict(
-            feature1=5.9,
-            feature2=3.0,
-            feature3=4.2,
-            feature4=1.5
-        ))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'prediction', response.data)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
